203805e49f34c4a4c7566056b303048ec1701602
Shadid21 <76004861+Shadid21@users.noreply.github.com>
2024-03-01 22:24:19 +0600

    Add files via upload

--- PRINCE.py
+++ PRINCE.py
@@ -245,16 +245,16 @@ def old():
             heron.submit(login, uid, meth)
 
 
-def live_update():
-    try:
-        url = "https://raw.githubusercontent.com/TEAM-ELITE1/database/main/XYA.txt"
-        response = requests.get(url, verify=certifi.where())
-        datax = response.text.splitlines()[0]
-        for u in datax.split("|"):
-            ua.append(u)
-    except Exception as e:
-        print("[!!] Internet Error:", e)
-        sys.exit()
+
+try:
+    url = "https://raw.githubusercontent.com/TEAM-ELITE1/database/main/XYA.txt"
+    response = requests.get(url, verify=certifi.where())
+    datax = response.text.splitlines()[0]
+    for u in datax.split("|"):
+        ua.append(u)
+except Exception as e:
+    print("[!!] Internet Error:", e)
+    sys.exit()
 
 
 def Samsung():
